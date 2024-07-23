83ec770164b1514d03f5876a068e8550546f889c
Olliefranks <156423860+Olliefranks@users.noreply.github.com>
2024-07-23 15:20:05 +0100

    first commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import Logo from './dojo-logo.png'
+
+export default function Navbar() {
+  return (
+    <div>
+
+        <nav>
+          <Image
+          src={Logo}
+          alt='Franks helpdesk logo'
+          width={70}
+          quality={100}
+          placeholder='blur'
+
+
+          
+          />
+          <h1> Ollie's Helpdesk </h1>
+          <Link href="/">Dashborard</Link>
+          <Link href="/tickets">Tickets</Link>
+        </nav>
+    </div>
+  )
+}
